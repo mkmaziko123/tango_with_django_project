cbcf54f911154e49f367d16e5ab353a8b3a86535
Morgan Mkambula <mmkambula1@gmail.com>
2026-02-12 19:14:27 +0000

    ok

--- rango/admin.py
+++ rango/admin.py
@@ -4,5 +4,5 @@ from rango.models import Page, Category
 # Define PageAdmin first
 class PageAdmin(admin.ModelAdmin):
     list_display = ('category', 'title', 'url')  # Fields to display in admin
-    admin.site.register(Category)
-    admin.site.register(Page, PageAdmin)  
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)  
