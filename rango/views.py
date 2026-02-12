fcd2f0ec57d3ffb51119d36ec3329ce27b475a26
Morgan Mkambula <mmkambula1@gmail.com>
2026-02-12 15:18:35 +0000

    exercise 1

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse(
+        "Rango says hey there partner! "
+        "<br/>"
+        "<a href='/rango/about/'>About</a>"
+    )
 from rango import views
 urlpatterns = [
 path('', views.index, name='index'),
 path('admin/', admin.site.urls),
 ]
+def about(request):
+    return HttpResponse(
+        "Rango says here is the about page."
+        "<br/>"
+        "<a href='/rango/'>Index</a>"
+    )
