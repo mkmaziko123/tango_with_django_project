ea23318cb3afd7c7b6816cae2e98b4957112e6c6
Morgan Mkambula <mmkambula1@gmail.com>
2026-02-13 13:54:08 +0000

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -32,26 +32,29 @@ def visitor_cookie_handler(request):
     return visits
 
 def index(request):
-    """Rango homepage view with server-side session-based visit counting."""
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-
     context_dict = {
         'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
         'categories': category_list,
-        'top_pages': page_list,
+        'pages': page_list
     }
 
-    visitor_cookie_handler(request)
-
-    context_dict['visits'] = request.session['visits']
+    visitor_cookie_handler(request)  # Update visit counter in session
+    
+    # Pass visits to the template but we won't display it on the homepage
+    context_dict['visits'] = request.session.get('visits', 1)
 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     """Render the about page."""
-    return render(request, 'rango/about.html')
-
+    visitor_cookie_handler(request) 
+    visits = request.session.get('visits', 1)  
+    context = {
+        'visits': visits,  
+    }
+    return render(request, 'rango/about.html', context)  # Render the about page
 
 def show_category(request, category_name_slug):
     """Display a single category and its pages."""
