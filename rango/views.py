de84b46ec59f5d8389e34d14abea935432edad04
Morgan Mkambula <mmkambula1@gmail.com>
2026-02-13 11:11:00 +0000

    exercises 7

--- rango/views.py
+++ rango/views.py
@@ -49,25 +49,35 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
-def add_page(request, category_name_slug):
-    """Handle adding a new page to a specific category."""
-    category = get_object_or_404(Category, slug=category_name_slug)
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from rango.models import Category, Page
+from rango.forms import PageForm
 
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    # Cannot add a page to a non-existent category
+    if category is None:
+        return redirect('rango:index')
+
+    form = PageForm()
     if request.method == 'POST':
         form = PageForm(request.POST)
         if form.is_valid():
             page = form.save(commit=False)
             page.category = category
+            page.views = 0
             page.save()
-            return redirect('rango:show_category', category_name_slug=category.slug)
+            return redirect(reverse('rango:show_category',
+                                    kwargs={'category_name_slug': category.slug}))
         else:
             print(form.errors)
-    else:
-        form = PageForm()
 
-    context = {
-        'form': form,
-        'category': category
-    }
-    return render(request, 'rango/add_page.html', context)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
+
 
