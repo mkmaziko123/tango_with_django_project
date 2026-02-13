980025257f7c1563dad4eb53d4647edf137d4c39
Morgan Mkambula <mmkambula1@gmail.com>
2026-02-13 10:13:41 +0000

    chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -37,7 +37,7 @@ def populate():
         {'title': 'Flask',
          'url': 'http://flask.pocoo.org'}
     ]
-     cats = {
+    cats = {
         'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
         'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
         'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from rango.models import Page, Category
+from rango.models import Category, Page
 
-# Define PageAdmin first
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('category', 'title', 'url')  # Fields to display in admin
-admin.site.register(Category)
-admin.site.register(Page, PageAdmin)  
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
